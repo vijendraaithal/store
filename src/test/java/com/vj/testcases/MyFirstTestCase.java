b46f7cb76cc05ca25c700189902b2cbda97fe303
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-07 17:37:01 +0530

    added depenedencies in pom.xml for selenium-java & testng

---
+++ src/test/java/com/vj/testcases/MyFirstTestCase.java
@@ -0,0 +1,15 @@
+package com.vj.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyFirstTestCase {
+
+    @Test
+    public void dummyTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.quit();
+    }
+}
