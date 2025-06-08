74bada7e2dc9fb985e838df0d458227a13b2e1eb
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-08 07:44:34 +0530

    Added BasePage: has private WebDriver variable and a constructor that assigns the passed driver to the instance driver variable

---
+++ src/test/java/com/vj/store/base/BasePage.java
@@ -0,0 +1,12 @@
+package com.vj.store.base;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    private WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
